3750ed8380225e8d5ff58611cbd284b8ccd0f24e
Floriane Maily <floriane.maily@gmail.com>
2023-11-29 23:24:16 +0100

    réalisation des boutons filtres terminée + changement color et background color au click sur chaque bouton + reset du style des autres boutons + ajout des commentaires

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,28 +1,4 @@
-
-
-fetch("http://localhost:5678/api/works")
-    .then(response => response.json())
-    .then(works => {
-        for (let i = 0; i < works.length; i++) {
-            const figure = works[i]
-            const gallery = document.querySelector(".gallery")
-            const workElement = document.createElement("figure")
-
-
-            const imageElement = document.createElement("img")
-            imageElement.src = figure.imageUrl
-            imageElement.alt = figure.title
-
-            const imageElementCaption = document.createElement("figcaption")
-            imageElementCaption.innerText = figure.title
-
-            gallery.appendChild(workElement)
-            workElement.appendChild(imageElement)
-            workElement.appendChild(imageElementCaption)
-        }
-        console.log(works)
-    })
-
+// Créations des 4 boutons filtres dans le DOM
 const sectionPortfolio = document.getElementById("portfolio")
 const btnFiltres = document.createElement("div")
 btnFiltres.classList.add("buttons")
@@ -47,11 +23,80 @@ btnFiltres.appendChild(btnAppartements)
 btnFiltres.appendChild(btnHotelsRestos)
 
 
-btnTous.addEventListener("click", () => {
+// Création des travaux dans le DOM + déclaration fonction genererWorks
+function genererWorks(works) {
+    const gallery = document.querySelector(".gallery")
+    gallery.innerHTML = ''
+    for (let i = 0; i < works.length; i++) {
+        const projects = works[i]
+        const workElement = document.createElement("figure")
+        const imageElement = document.createElement("img")
+        imageElement.src = projects.imageUrl
+        imageElement.alt = projects.title
+        const imageElementCaption = document.createElement("figcaption")
+        imageElementCaption.innerText = projects.title
+
+        gallery.appendChild(workElement)
+        workElement.appendChild(imageElement)
+        workElement.appendChild(imageElementCaption)
+    }
+}
 
 
+// Récupération des travaux via l'API + appel de la fonction genererWorks
+fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(data => {
+        works = data
+        genererWorks(works)
+    })
 
+// Sélection de tous les boutons filtres et déclaration de la fonction resetColors 
+const buttons = document.querySelectorAll("button")
+function resetColors() {
+    buttons.forEach(button => {
+        button.style.color = "initial"
+        button.style.backgroundColor = "initial"
+    })
+}
 
+// Au clic sur un bouton : le style change et les autres boutons reprennent le style initial avec l'appel de la fonction resetColors
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        resetColors()
+        button.style.color = "#FFFEF8"
+        button.style.backgroundColor = "#1D6154"
+    })
 })
+
+
+// Au clic sur chaque bouton : filtre des travaux par catégorie 
+btnTous.addEventListener("click", () => {
+    console.log(works)
+    genererWorks(works)
+})
+
+btnObjets.addEventListener("click", () => {
+    const objets = works.filter(work => work.categoryId === 1)
+    console.log(objets)
+    genererWorks(objets)
+})
+
+btnAppartements.addEventListener("click", () => {
+    const appartements = works.filter(work => work.categoryId === 2)
+    console.log(appartements)
+    genererWorks(appartements)
+})
+btnHotelsRestos.addEventListener("click", () => {
+    const hotelRestos = works.filter(work => work.categoryId === 3)
+    console.log(hotelRestos)
+    genererWorks(hotelRestos)
+})
+
+
+
+
+
+
 
 
