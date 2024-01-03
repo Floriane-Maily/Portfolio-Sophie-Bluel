d5c9f996e281b674545d44c9a1a26dd2ab7812e1
Floriane Maily <floriane.maily@gmail.com>
2024-01-03 19:00:12 +0100

    connexion administrateur : erreur 401 acces non autorisé

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -2,25 +2,30 @@
 const loginForm = document.querySelector(".login-form")
 loginForm.addEventListener("submit", function (event) {
     event.preventDefault()
-
 })
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
 const url = "http://localhost:5678/api/users/login"
 const data = {
     email: "sophie.bluel@test.tld",
     password: "S0phie"
 }
-fetch(url, {
+
+const response = await fetch(url, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+    },
     body: JSON.stringify(data)
 })
-
     .then(response => response.json())
     .then(data => console.log(data))
     .catch((error) => {
         console.error('Error:', error);
     })
+
+window.localStorage.setItem("token", token)
 
 
 
