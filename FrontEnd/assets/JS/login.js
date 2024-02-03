1666c6ec8e8ee567618ed7a7a42346b71740fb29
Floriane Maily <floriane.maily@gmail.com>
2024-02-03 15:37:23 +0100

    dernières modifs effectuées, je dois encore créer le message d'erreur et revoir le code de login.JS

--- FrontEnd/assets/JS/login.js
+++ FrontEnd/assets/JS/login.js
@@ -1,39 +1,36 @@
 
+
 const loginForm = document.querySelector(".login-form")
-loginForm.addEventListener("submit", function (event) {
-    event.preventDefault()
-})
-
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR - M - 5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
 const url = "http://localhost:5678/api/users/login"
-const data = {
-    email: "sophie.bluel@test.tld",
-    password: "S0phie"
-}
+const emailLogin = document.getElementById("email").value
+const passwordLogin = document.getElementById("password").value
 
-fetch(url, {
-    method: "POST",
-    headers: {
-        'accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-    },
-    body: JSON.stringify(data)
-})
-    .then(response => {
-        if (response.ok) {
-            return response.json()
-        }
-        else {
+
+
+
+loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault()
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(emailLogin, passwordLogin)
+        })
+
+        if (response.status === 200) {
+            const data = await response.json()
+            localStorage.setItem("token", token)
+        } else {
             throw new Error(response.status)
         }
-    })
-    .then(data => console.log(data))
-localStorage.setItem("token", token)
-    .catch((error) => {
-        console.error('erreur', error)
-    })
 
+    } catch (error) {
+        console.error('erreur', error)
+    }
+})
 
 
 
