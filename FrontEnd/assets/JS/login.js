58b0bcbbfda73afdc6a6527a279dc6dac4d464e0
Floriane Maily <floriane.maily@gmail.com>
2024-02-27 00:20:00 +0100

    page d'accueil en mode édition: ajout des éléments dans le html et le css

--- FrontEnd/assets/JS/login.js
+++ FrontEnd/assets/JS/login.js
@@ -1,11 +1,8 @@
 
-
 const loginForm = document.querySelector(".login-form")
 const url = "http://localhost:5678/api/users/login"
-const emailLogin = document.getElementById("email").value
-const passwordLogin = document.getElementById("password").value
-
-
+const emailLogin = document.getElementById("email")
+const passwordLogin = document.getElementById("password")
 
 
 loginForm.addEventListener("submit", async function (event) {
@@ -17,18 +14,22 @@ loginForm.addEventListener("submit", async function (event) {
                 'accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(emailLogin, passwordLogin)
+            body: JSON.stringify({
+                email: emailLogin.value,
+                password: passwordLogin.value
+            })
         })
 
         if (response.status === 200) {
             const data = await response.json()
-            localStorage.setItem("token", token)
+            localStorage.setItem("token", data.token)
+            window.location.href = "http://127.0.0.1:5500/FrontEnd/index.html"
         } else {
             throw new Error(response.status)
         }
 
     } catch (error) {
-        console.error('erreur', error)
+        alert("La combinaison email/mot de passe est invalide", error)
     }
 })
 
